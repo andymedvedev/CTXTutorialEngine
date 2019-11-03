794f2b5dfb762b507ae17d3e90a5a85a88fc3d8b
Andrey Medvedev <andrey.medvedev@it-olimp-tomsk.com>
2019-11-03 14:18:32 +0700

    remove casting in config loader

--- CTXTutorialEngine/Engine/Utlis/CTXTutorialConfigLoader.swift
+++ CTXTutorialEngine/Engine/Utlis/CTXTutorialConfigLoader.swift
@@ -16,6 +16,7 @@ enum ConfigLoaderError: Error {
 final class CTXTutorialConfigLoader {
     
     func loadConfigs<M: Meta>(from fileName: String, eventConfigMetaType: M.Type) throws -> [CTXTutorialConfig<M>]  {
+        
         guard let configFileURL = Bundle.main.url(forResource: fileName, withExtension: "json") else {
             throw ConfigLoaderError.error("Config with name: \"\(fileName)\" not found.")
         }
@@ -26,7 +27,7 @@ final class CTXTutorialConfigLoader {
         
         configs = try decoder.decode([String: [CTXTutorialConfig<M>]].self, from: data)
         
-        if let configs = configs?["tutorials"] as? [CTXTutorialConfig<M>] {
+        if let configs = configs?["tutorials"] {
             return configs
         } else {
             throw ConfigLoaderError.error("Configs are nil")
